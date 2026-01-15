9dc5f7a5714df3c9ad0a493ffccbf9c2f1564b28
Javier <javier.candela.berrocal@gmail.com>
2026-01-15 16:05:05 +0100

    Adding cloudwatch endpoint to allow ECS communicate with CloudWatch

--- network/main.tf
+++ network/main.tf
@@ -110,3 +110,13 @@ resource "aws_vpc_endpoint" "s3" {
   vpc_endpoint_type = "Gateway"
   route_table_ids   = [aws_route_table.private_route_table.id]
 }
+
+# CloudWatch Logs Endpoint
+resource "aws_vpc_endpoint" "logs" {
+  vpc_id              = aws_vpc.septa_vpc.id
+  service_name        = "com.amazonaws.eu-west-1.logs"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+  subnet_ids          = [for s in aws_subnet.private_subnet : s.id]
+  security_group_ids  = [aws_security_group.allow_private_access.id]
+}
