5471820f5e87a8f999a33f36ca80255c75e80428
Javier <javier.candela.berrocal@gmail.com>
2026-01-15 16:50:50 +0100

    Association public subnet with public route table

--- network/main.tf
+++ network/main.tf
@@ -48,9 +48,16 @@ resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.septa_vpc.id
 
   route {
-    cidr_block = var.client_ip
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.septa_igw.id
   }
+}
+
+# Associate Public Subnets with Public Route Table
+resource "aws_route_table_association" "public_subnet_association" {
+  for_each       = aws_subnet.public_subnet
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public_route_table.id
 }
 
 # Ingress Rule to Allow Public HTTP Access
