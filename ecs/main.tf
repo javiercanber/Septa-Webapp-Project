19d53b9e898b1f202b427b5be304e0b13595b5aa
Javier <javier.candela.berrocal@gmail.com>
2026-01-15 14:08:40 +0100

    Adding task_definition to create ECS Containers

--- ecs/main.tf
+++ ecs/main.tf
@@ -7,3 +7,52 @@ resource "aws_ecs_cluster" "septa_ecs_cluster" {
     value = "enabled"
   }
 }
+
+resource "aws_ecs_task_definition" "septa_task_definition" {
+  family                   = "septa-task-webapp"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "septa-webapp"
+      image     = "${aws_ecr_repository.septa_ecr_repo.repository_url}:septa-webapp1.0.0"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+          protocol      = "tcp"
+        }
+      ]
+      
+       logConfiguration = {
+          logDriver = "awslogs"
+          options = {
+            "awslogs-group"         = "/ecs/septa-webapp-logs"
+            "awslogs-region"        = var.region
+            "awslogs-stream-prefix" = "ecs"
+          }
+        }
+    }
+  ])
+}
+
+# ECS Service for Septa Webapp
+resource "aws_ecs_service" "septa_ecs_service" {
+  name            = "septa-ecs-service"
+  cluster         = aws_ecs_cluster.septa_ecs_cluster.id
+  task_definition = aws_ecs_task_definition.septa_task_definition.arn
+  launch_type     = "FARGATE"
+  desired_count   = 2
+
+  network_configuration {
+    subnets         = [for s in aws_subnet.private_subnet : s.id]
+    security_groups = [aws_security_group.allow_private_access.id]
+    assign_public_ip = false
+  }
+}
--- ecs/variables.tf
+++ ecs/variables.tf
@@ -2,3 +2,8 @@ variable "private_subnet_cidr_block" {
   type        = list(string)
   description = "IP address range for the subnet"
 }
+
+variable "region" {
+  type = string
+  description = "AWS Region"
+}
